e86710abd6e44be31b737bc33f3f4dea5b7da53a
koala31BM <kenneger31@gmail.com>
2026-03-17 18:52:27 -0600

    se implementa savevehicle para el guardado correcto de la informacion del carro y para poder subir la imagen

--- js/index.js
+++ js/index.js
@@ -26,20 +26,60 @@ function renderVehicles(list) {
     let html = '';
 
     list.forEach(vehicle => {
+
+        const imageUrl = vehicle.image
+            ? `http://localhost:3008/upload/${vehicle.image}`
+            : "https://via.placeholder.com/400";
         html += `
             <div class="vehicle-card">
+
+                <img src="${imageUrl}" class="vehicle-img">
+
                 <h3>${vehicle.brand} ${vehicle.model}</h3>
+>
+                <p class="desc">
+                    ${vehicle.description || "Sin descripción"}
+                </p>
 
                 <p><b>Año:</b> ${vehicle.year}</p>
                 <p><b>Precio:</b> $${vehicle.price}</p>
 
-                <button onclick="viewVehicle('${vehicle.id}')">Ver Detalle</button>
+                <button onclick="viewVehicle('${vehicle._id}')">
+                    Ver Detalle
+                </button>
+
+             
+
             </div>
         `;
     });
 
     container.innerHTML = html;
 }
+
+    async function saveVehicle() {
+
+    const formData = new FormData();
+
+    formData.append("brand", document.getElementById("brand").value);
+    formData.append("model", document.getElementById("model").value);
+    formData.append("year", document.getElementById("year").value);
+    formData.append("price", document.getElementById("price").value);
+    formData.append("description", document.getElementById("description").value);
+
+    const fileInput = document.getElementById("image");
+    formData.append("image", fileInput.files[0]);
+
+    await fetch("http://localhost:3008/api/vehicles", {
+        method: "POST",
+        body: formData
+    });
+
+    alert("Vehículo guardado");
+}
+
+    container.innerHTML = html;
+
 
 function searchVehicles() {
     const text = document.getElementById('searchInput').value.toLowerCase();
