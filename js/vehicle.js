af48d5ca7017dd12641e58e9893ee08755d27cab
Eliam Araya <eliamaa02@gmail.com>
2026-03-18 15:03:50 -0600

    Union de archivos

--- js/vehicle.js
+++ js/vehicle.js
@@ -4,8 +4,50 @@ window.onload = function () {
     getVehicles();
 };
 
-// Obtener vehículos
-async function getVehicles() {
+async function createVehicle(){
+
+const brand = document.getElementById("brand").value;
+const model = document.getElementById("model").value;
+const year = document.getElementById("year").value;
+const price = document.getElementById("price").value;
+
+const token = sessionStorage.getItem("authToken");
+
+const response = await fetch(`${API_BASE}/vehicle`, {
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+"Authorization": `Bearer ${token}`
+},
+body: JSON.stringify({
+brand,
+model,
+year,
+price
+})
+});
+
+if(response.ok){
+
+alert("Vehículo guardado correctamente");
+
+document.getElementById("brand").value="";
+document.getElementById("model").value="";
+document.getElementById("year").value="";
+document.getElementById("price").value="";
+
+getVehicles();
+
+}else{
+
+alert("No se pudo crear el vehículo");
+
+}
+
+}
+
+
+async function getVehicles(){
 
     const token = sessionStorage.getItem("authToken");
 
@@ -43,4 +85,21 @@ async function getVehicles() {
 
 function viewVehicle(id) {
     window.location.href = `VehicleDetail.html?id=${id}`;
+
+let html = "";
+
+vehicles.forEach(v => {
+
+html += `
+<tr>
+<td>${v.brand}</td>
+<td>${v.model}</td>
+<td>${v.year}</td>
+<td>${v.price}</td>
+</tr>
+`;
+
+});
+
+document.getElementById("vehicleList").innerHTML = html;
 }
